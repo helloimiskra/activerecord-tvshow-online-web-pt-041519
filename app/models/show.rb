d8b7c59c70bc6b3969f4ec71c1c2826b38cf2ac9
Iskra B <ibatistapoblete@gmail.com>
2019-07-12 04:09:58 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,4 +7,17 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     self.find_by(rating: highest_rating )
   end
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    self.find_by(rating:lowest_rating)
+  end
+
+  def shows_by_alphabetical_order
+    self.order
+  end
+
 end
